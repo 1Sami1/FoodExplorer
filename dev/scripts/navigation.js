fc1330b2993dcd0e873e48f39c1a28e9e1be4d22
Adam Hauck <ad.hauck@gmail.com>
2022-04-13 11:23:07 -0700

    Nav Menu Button actions

--- dev/scripts/navigation.js
+++ dev/scripts/navigation.js
@@ -1,14 +1,31 @@
-// var button = document.getElementById("button");
+var button = document.getElementById("button");
 
-// button.addEventListener("click",myFunction());
+button.addEventListener("click",myFunction);
+
+window.onresize = myFunction2;
 
 
 
-// function myFunction() {
-// 	var x = document.getElementById("header-menu");
-// 	if (x.style.display === "none") {
-// 	  x.style.display = "block";
-// 	} else {
-// 	  x.style.display = "none";
-// 	}
-//   }
+function myFunction() {
+	var x = document.getElementById("header-menu");
+    if(window.innerWidth < 500){
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+    } 
+    // else {
+    //     x.style.display = "flex";
+    // }
+  }
+
+  function myFunction2() {
+      var x = document.getElementById("header-menu");
+      if(window.innerWidth >499) {
+          x.style.display = "flex";
+        
+      } else {
+          x.style.display = "none";
+      }
+  }
